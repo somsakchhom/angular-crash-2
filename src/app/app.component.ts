938e91f24fae7a2d50b1656f0cdc8eca68cb07cd
Somsak Chhom <somsak.chhom@gmail.com>
2023-01-14 14:48:28 -0800

    updated CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
+  
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// methods or properties added below, custom, lifecycle
 export class AppComponent {
-  title = 'angular-crash-2';
+  // suggested adding types to your properties, functions, 'title' is now'title: string'
+  title: string = 'Task Tracker';
 }
