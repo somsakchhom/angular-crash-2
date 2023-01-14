02c4f36cee1d7f85230c0e22261cd899545a888c
Somsak Chhom <somsak.chhom@gmail.com>
2023-01-14 15:17:34 -0800

    video21:52 export class does not matching on local, created new branch called second-branch

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+// in the video 20:38: 
+//  export class HeaderComponent implements OnInit {
+//  constructor() {}
+//  ngOnInit(): void {}
+// }
+export class HeaderComponent {
+
+}
