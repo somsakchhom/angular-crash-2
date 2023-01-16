d9aa59e2c008b7023f176dd06449a3a6eb94d4a2
Somsak Chhom <somsak.chhom@gmail.com>
2023-01-16 12:32:13 -0800

    23:31 added button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule
