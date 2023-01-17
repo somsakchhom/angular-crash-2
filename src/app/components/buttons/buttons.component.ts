bfaedc683ae837e9cc63b0a7daa01382481c3020
Somsak Chhom <somsak.chhom@gmail.com>
2023-01-17 12:41:11 -0800

    Added button and button color

--- src/app/components/buttons/buttons.component.ts
+++ src/app/components/buttons/buttons.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-buttons',
   templateUrl: './buttons.component.html',
   styleUrls: ['./buttons.component.css']
 })
-export class ButtonsComponent {
+export class ButtonsComponent implements OnInit {
+  @Input() text: string = '';
+  @Input() color: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+
+
 
 }
